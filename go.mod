7f346b258bb3de412f989e5afe57b7124afc55fa
akrck02 <aketza.vazquez@gmail.com>
2024-05-22 18:34:30 +0200

    adding endpoints

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/akrck02/valhalla-user-api
 go 1.21.8
 
 require (
-	github.com/akrck02/valhalla-api-common v0.0.5
+	github.com/akrck02/valhalla-api-common v0.0.8
 	github.com/akrck02/valhalla-core-dal v0.0.4
-	github.com/akrck02/valhalla-core-sdk v0.0.7
+	github.com/akrck02/valhalla-core-sdk v0.0.8
 	github.com/gin-gonic/gin v1.9.1
 )
 
--- main.go
+++ main.go
@@ -16,7 +16,32 @@ func main() {
 	apicommon.Start(
 		config,
 		[]models.Endpoint{
-			models.Endpoint{Path: "register", Method: http.HTTP_METHOD_PUT, Listener: services.RegisterHttp},
+
+			// User endpoints
+			{Path: "register", Method: http.HTTP_METHOD_PUT, Listener: services.RegisterHttp},
+			{Path: "login", Method: http.HTTP_METHOD_POST, Listener: services.LoginHttp},
+			{Path: "login/auth", Method: http.HTTP_METHOD_POST, Listener: services.LoginAuthHttp},
+			{Path: "edit", Method: http.HTTP_METHOD_PATCH, Listener: services.EditUserHttp},
+			{Path: "edit/email", Method: http.HTTP_METHOD_PATCH, Listener: services.EditUserEmailHttp},
+			{Path: "edit/profilepic", Method: http.HTTP_METHOD_PATCH, Listener: services.EditUserProfilePictureHttp},
+			{Path: "delete", Method: http.HTTP_METHOD_DELETE, Listener: services.DeleteUserHttp},
+			{Path: "get", Method: http.HTTP_METHOD_GET, Listener: services.GetUserHttp},
+			{Path: "validate", Method: http.HTTP_METHOD_GET, Listener: services.ValidateUserHttp},
+
+			// Role endpoints
+			{Path: "role/create", Method: http.HTTP_METHOD_PUT, Listener: services.CreateRoleHttp},
+			{Path: "role/delete", Method: http.HTTP_METHOD_DELETE, Listener: services.DeleteRoleHttp},
+			{Path: "role/get", Method: http.HTTP_METHOD_GET, Listener: services.GetRoleHttp},
+			{Path: "role/edit", Method: http.HTTP_METHOD_PATCH, Listener: services.EditRoleHttp},
+
+			// Team endpoints
+			{Path: "team/create", Method: http.HTTP_METHOD_PUT, Listener: services.CreateTeamHttp},
+			{Path: "team/delete", Method: http.HTTP_METHOD_DELETE, Listener: services.DeleteTeamHttp},
+			{Path: "team/get", Method: http.HTTP_METHOD_GET, Listener: services.GetTeamHttp},
+			{Path: "team/edit", Method: http.HTTP_METHOD_PATCH, Listener: services.EditTeamHttp},
+			{Path: "team/edit/owner", Method: http.HTTP_METHOD_PUT, Listener: services.EditTeamOwnerHttp},
+			{Path: "team/add/member", Method: http.HTTP_METHOD_PUT, Listener: services.AddMemberHttp},
+			{Path: "team/remove/member", Method: http.HTTP_METHOD_DELETE, Listener: services.RemoveMemberHttp},
 		},
 	)
 
